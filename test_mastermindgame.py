d28830080f21c5bd6617c44116e799baa46a7d47
Eduardo Ubide <eduardo.ubide@audiense.com>
2023-12-04 16:49:10 +0100

    Add some test

--- test_mastermindgame.py
+++ test_mastermindgame.py
@@ -1,5 +1,6 @@
+from automata import Automata
 import pytest
-from mastermindgame import MastermindGame
+from mastermindgame import MastermindGame, Automata
 import uuid
 
 
@@ -28,8 +29,28 @@ def test_iteration_returns_correct_results():
     assert result == (game.game_id, 1, [1, 2, 3, 4], [1, 2, 3, 4], 4)
 
 
-# def test_play():
-#     game = MastermindGame()
-#     game.secret_code = [1, 2, 3, 4]
-#     with pytest.raises(SystemExit):
-#         game.play()
+def test_play():
+    game = MastermindGame()
+    game.secret_code = [1, 2, 3, 4]
+    with pytest.raises(SystemExit):
+        game.play()
+
+
+def test_generate_code_returns_4_digits():
+    automata = Automata()
+    code = automata.generate_code()
+    assert len(code) == 4
+
+
+def test_generate_code_returns_numbers_from_1_to_6():
+    automata = Automata()
+    code = automata.generate_code()
+    for digit in code:
+        assert 1 <= digit <= 6
+
+
+def test_generate_code_returns_different_codes():
+    automata = Automata()
+    code1 = automata.generate_code()
+    code2 = automata.generate_code()
+    assert code1 != code2
