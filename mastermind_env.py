12a585e52fcd7e4ea1c6272b4fbd93b0776f130b
Eduardo Ubide <eubide@gmail.com>
2023-12-16 17:15:17 +0100

    Improve the loop logic

--- mastermind_env.py
+++ mastermind_env.py
@@ -4,42 +4,61 @@ import random
 import os
 from mastermind_agent import Agent
 
-random.seed(0)
+# random.seed(0)
 
 
 def generate_random_code():
     return [random.randint(1, 6) for _ in range(4)]
 
 
+def flatten_list(nested_list):
+    flat_list = []
+    for element in nested_list:
+        if isinstance(element, list):
+            flat_list.extend(element)
+        else:
+            flat_list.append(element)
+    return flat_list
+
+
 class Mastermind:
     def __init__(self):
+
+        self.DIRECTORY = "games"
+
         self.MAX_ATTEMPTS = 10
         self.game_id = str(uuid.uuid4())
         self.secret_code = generate_random_code()
         self.attempts = 0
-        self.movements = []
         self.win = False
         self.state = [[[0, 0, 0, 0], 0, 0]
                       for _ in range(self.MAX_ATTEMPTS)]
 
-    def print_state(self):
-        for row in self.state:
-            print(row)
+    def print_state(self, debug=False):
+        if self.win:
+
+            if not os.path.exists(self.DIRECTORY):
+                os.makedirs(self.DIRECTORY)
+            filepath = os.path.join(self.DIRECTORY, self.game_id+".txt")
+            with open(filepath, "w") as f:
+                for row in self.state:
+                    flat_row = flatten_list(row)
+                    numbers_str = ', '.join(map(str, flat_row))
+                    f.write(str(numbers_str)+"\n")
+
+        if debug:
+            if self.win:
+                print(f"You win! in {self.attempts} attempts")
+            else:
+                print("You lose!")
+                print(f"Secret code: {self.secret_code}")
+            for row in self.state:
+                flat_row = flatten_list(row)
+                print(flat_row)
 
     def add_movement_to_state(self, user_code, correct_position, correct_digit):
         self.state[self.attempts] = [
             user_code, correct_position, correct_digit]
-
-    def print_game(self):
-        if self.win == 4 and self.attempts > 3:
-            print(f"You win in {self.attempts}!")
-            directory = "games"
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            filepath = os.path.join(directory, self.game_id+".txt")
-            with open(filepath, "w") as f:
-                for movement in self.movements:
-                    f.write(str(movement)+"\n")
 
     def evalue_user_code(self, user_code):
         if user_code == self.secret_code:
@@ -50,7 +69,8 @@ class Mastermind:
         correct_digit = sum(min(user_code.count(
             digit), self.secret_code.count(digit)) for digit in set(user_code))
 
-        self.attempts += 1
+        if correct_position == 4:
+            self.win = True
         return self.game_id, self.attempts, self.secret_code, user_code, correct_position, correct_digit
 
     def game(self):
@@ -59,15 +79,13 @@ class Mastermind:
         while True:
             user_code = my_agent.generate_next_code(self.state)
             result = self.evalue_user_code(user_code)
-            self.movements.append(result)
-            self.print_state()
-            self.add_movement_to_state(result[3], result[4], result[5])
-            self.win = result[4]
+            self.add_movement_to_state(user_code, result[4], result[5])
 
-            if self.win == 4 or self.attempts == self.MAX_ATTEMPTS:
-                self.print_game()
-                self.print_state()
-                return
+            self.attempts += 1
+
+            if self.win or self.attempts == self.MAX_ATTEMPTS:
+                self.print_state(debug=True)
+                return self.win
 
 
 game = Mastermind()
